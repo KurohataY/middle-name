64ed2c16e6a3b92d1265156d24d39a5398c2af7d
‘KurohataY’ <shihata001@gmail.com>
2019-06-01 13:58:05 +0000

    ランダムデータベース検索メソッドの追加

--- db/migrate/20190531044534_create_middles.rb
+++ db/migrate/20190531044534_create_middles.rb
@@ -1,10 +1,10 @@
 class CreateMiddles < ActiveRecord::Migration[5.0]
   def change
     create_table :middles do |t|
-      t.references :mr, foreign_key: true
-      t.references :surname, foreign_key: true
+      t.string :mr
+      t.string :surname
       t.string :Lname
-      t.references :commonname, foreign_key: true
+      t.string :commonname
       t.string :Fname
       t.timestamps
     end
