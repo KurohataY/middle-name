6dd5bc97d8805637f107594a2fe6772fb4f8dc1c
Kurohata <ec2-user@ip-172-31-64-138.ec2.internal>
2019-06-15 07:47:04 +0000

    ランキング作成

--- app/models/mr.rb
+++ app/models/mr.rb
@@ -2,8 +2,7 @@ class Mr < ApplicationRecord
   validates :mr, presence: true, length: { maximum: 255 }
   
   def self.ranking
-    self.group(:mr_id).order('count_mr_id').limit(2).count(:mr_id)
+    Middle.group(:mr_id).order('count_mr_id DESC').limit(2).count(:mr_id)
   end
-  
-  
+
 end
--- app/models/surname.rb
+++ app/models/surname.rb
@@ -2,7 +2,7 @@ class Surname < ApplicationRecord
   validates :surname, presence: true, length: { maximum: 255 }
   
   def self.ranking
-    self.group(:surname_id).order('count_surname_id').limit(10).count(:surname_id)
+    Middle.group(:surname_id).order('count_surname_id').limit(5).count(:surname_id)
   end
   
   
