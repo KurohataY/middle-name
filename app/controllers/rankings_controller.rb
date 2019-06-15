75321b59b40558a23b4f3c15c3ca35da91669f94
‘KurohataY’ <shihata001@gmail.com>
2019-06-15 07:47:04 +0000

    ランキング作成

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,12 +1,12 @@
 class RankingsController < ApplicationController
   def middle
     @ranking_mr_counts = Mr.ranking
-    @ranking_surname_counts = Surname.ranking
-    @ranking_commonname_counts = Commonname.ranking
-    
     @mrs = Mr.find(@ranking_mr_counts.keys)
-    @surnames = Surnames.find(@ranking_surname_counts.keys)
-    @commonnames = Commonname.find(@ranking_commonname_counts.keys)
     
+    @ranking_surname_counts = Surname.ranking
+    @surnames = Surname.find(@ranking_surname_counts.keys)
+    
+    @ranking_commonname_counts = Commonname.ranking
+    @commonnames = Commonname.find(@ranking_commonname_counts.keys)
   end
 end
--- app/models/commonname.rb
+++ app/models/commonname.rb
@@ -1,9 +1,6 @@
 class Commonname < ApplicationRecord
   validates :commonname, presence: true, length: { maximum: 255 }
-  
   def self.ranking
-    self.group(:commonname_id).order('count_commonname_id').limit(10).count(:commonname_id)
+    Middle.group(:commonname_id).order('count_commonname_id').limit(5).count(:commonname_id)
   end
-  
-
 end
