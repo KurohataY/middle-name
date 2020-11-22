3379c2cb1fed46cb9b2f41df2d48a75087ee4dd1
‘KurohataY’ <shihata001@gmail.com>
2020-11-23 02:48:39 +0900

    docker docker-compose更新

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,6 @@ rm -f /middle-name/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
+
+# rails initdb
+rails db:create db:migrate db:seed
